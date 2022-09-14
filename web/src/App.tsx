c82b7d5a2edf8bac6fa0ef3e2e54e9824300dfd6
Adriano Santos <adrianodrix@gmail.com>
2022-09-14 14:26:12 -0300

    feat: init landing page

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,21 +1,13 @@
+import './styles/main.css';
+import logoImg from './assets/nlw-esports-logo.svg';
 
-interface ButtonProps {
-  title: string;
-}
-
-function Button(props: ButtonProps) {
-  return (
-    <button>
-      {props.title}
-    </button>
-  )
-}
 function App() {
   return (
-    <div>
-      <Button title='Send 1'/>
-      <Button title='Send 2'/>
-      <Button title='Send 3'/>
+    <div className='max-w-[1344px] mx-auto flex flex-col items-center my-20'>
+      <img src={logoImg} alt="NLW ESports" />
+      <h1 className='text-6xl text-white font-black mt-20'
+        >Seu <span className='text-transparent bg-nlw-gradient bg-clip-text' >duo</span> esta aqui.
+      </h1>
     </div>
   )
 }
