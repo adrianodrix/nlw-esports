439069ff517eb5b6992857503bd3287f5a2dba02
Adriano Santos <adrianodrix@gmail.com>
2022-09-14 22:24:49 -0300

    feat: list of games

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -5,9 +5,61 @@ function App() {
   return (
     <div className='max-w-[1344px] mx-auto flex flex-col items-center my-20'>
       <img src={logoImg} alt="NLW ESports" />
+
       <h1 className='text-6xl text-white font-black mt-20'
         >Seu <span className='text-transparent bg-nlw-gradient bg-clip-text' >duo</span> esta aqui.
       </h1>
+
+      <div className='grid grid-cols-6 gap-6 mt-16'>
+        <a href="" className='relative rounded-lg overflow-hidden'>
+          <img src="/game1.png" alt="" />
+
+          <div className='w-full pt-16 pb -4 px-4 bg-game-grandient absolute bottom-0 left-0 right-0'>
+            <strong className='font-bold text-white block'>League of Legends</strong>
+            <span className='text-zinc-300 text-sm block mt-1'>4 anúncios</span>
+          </div>
+        </a>     
+        <a href="" className='relative rounded-lg overflow-hidden'>
+          <img src="/game2.png" alt="" />
+
+           <div className='w-full pt-16 pb-4 px-4 bg-game-grandient absolute bottom-0 left-0 right-0'>
+            <strong className='font-bold text-white block'>Dota II</strong>
+            <span className='text-zinc-300 text-sm block mt-1'>2 anúncios</span>
+          </div> 
+        </a>
+        <a href="" className='relative rounded-lg overflow-hidden'>
+          <img src="/game3.png" alt="" />
+
+           <div className='w-full pt-16 pb-4 px-4 bg-game-grandient absolute bottom-0 left-0 right-0'>
+            <strong className='font-bold text-white block'>CS GO</strong>
+            <span className='text-zinc-300 text-sm block mt-1'>8 anúncios</span>
+          </div> 
+        </a>
+        <a href="" className='relative rounded-lg overflow-hidden'>
+          <img src="/game4.png" alt="" />
+
+           <div className='w-full pt-16 pb-4 px-4 bg-game-grandient absolute bottom-0 left-0 right-0'>
+            <strong className='font-bold text-white block'>Apex Legends</strong>
+            <span className='text-zinc-300 text-sm block mt-1'>52 anúncios</span>
+          </div> 
+        </a>
+        <a href="" className='relative'>
+          <img src="/game5.png" alt="" />
+
+          <div className='w-full pt-16 pb-4 px-4 bg-game-grandient absolute bottom-0 left-0 right-0'>
+            <strong className='font-bold text-white block'>Fortnite</strong>
+            <span className='text-zinc-300 text-sm block mt-1'>8 anúncios</span>
+          </div> 
+        </a>
+        <a href="" className='relative rounded-lg overflow-hidden'>
+          <img src="/game6.png" alt="" />
+
+          <div className='w-full pt-16 pb-4 px-4 bg-game-grandient absolute bottom-0 left-0 right-0'>
+            <strong className='font-bold text-white block'>World of Warcraft</strong>
+            <span className='text-zinc-300 text-sm block mt-1'>4 anúncios</span>
+          </div> 
+        </a>        
+      </div>
     </div>
   )
 }
--- web/tailwind.config.cjs
+++ web/tailwind.config.cjs
@@ -11,7 +11,8 @@ module.exports = {
       },
       backgroundImage: {
         galaxy: "url('/background-galaxy.png')",
-        'nlw-gradient': 'linear-gradient(89.86deg, #9572FC 20%, #43E7AD 60%, #E1D55D 20%)'
+        'nlw-gradient': 'linear-gradient(89.86deg, #9572FC 20%, #43E7AD 60%, #E1D55D 20%)',
+        'game-grandient': 'linear-gradient(180deg, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.9) 67.08%)'
       }
     },
   },
